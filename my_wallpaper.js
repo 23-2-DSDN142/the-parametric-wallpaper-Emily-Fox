ed7eb0110227d3bea76bd08f8fbb6c5403e9cb0f
thecraftysheilder <150868563+thecraftysheilder@users.noreply.github.com>
2023-11-24 15:49:34 +1300

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,23 +1,125 @@
-//your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+//setup - run once when the code is first starts
+function setup() {
+  createCanvas(1080, 720); // this sets the size of the area in which we can draw things. We call it a canvas
+  textSize(32); // sets the size of the textAscent()
 
-
-function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  pWallpaper.resolution(FIT_TO_SCREEN);
-  pWallpaper.show_guide(true); //set this to false when you're ready to print
-
-  //Grid settings
-  pWallpaper.grid_settings.cell_width  = 200;
-  pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
 }
+//draw - this function loops, the code within read through repeatedly while running
+function draw() {
+  clear(); // resets all elements on screen
+  background(255); // this makes the whole canvas the screen grey
+  var triH = 60;
+  var triW = triH/2;
+  var fliped = false;
+  var flipNext = true;
+  var allign = true;
+  var spaces = false;
+  var square = false;
+  noStroke();
+  var red=random(0,255);
+  var green=random(0,0);
+  var blue=random(red,red);
+  fill(red,green,blue);
+  stroke(red,green,blue);
+  //text("x pos is " + mouseX, 50, 50); // displays text, including a P5.js variable which tracks the mouse position
+  //text("y pos is " + mouseY, 50, 80);
 
-function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  //ellipse(mouseX, mouseY, 20, 20); // draws a circle 20 pixels accross at location of the mouse pointer
+  //line(mouseX, mouseY, pmouseX, pmouseY); // draws a line from the mouse pointer, to where the mouse pointer previously was
+  for (var j=0;j<720/triH;j++){
+    for (var i=0;i<1080/triW;i++){
+      if (!fliped){
+        if (flipNext){
+          beginShape();
+          vertex(i*triW,j*triH);
+          vertex(triW+(i*triW),(j*triH));
+          vertex((i*triW),triH+(j*triH));
+          vertex(i*triW,j*triH);
+          endShape();
+          flipNext=false;
+          fliped=true;
+          if (!spaces)i--;
+          if (!square){
+          var red=random(0,255);
+          var green=random(0,0);
+          var blue=random(red,red);
+          fill(red,green,blue);
+          stroke(red,green,blue);
+          }
+        }
+        else{
+          beginShape();
+          vertex(triW+(i*triW),(j*triH));
+          vertex(i*triW,j*triH);
+          vertex(triW+(i*triW),triH+(j*triH));
+          vertex(triW+(i*triW),(j*triH));
+          endShape();
+          flipNext=true;
+          if (square){
+            var red=random(0,255);
+            var green=random(0,0);
+            var blue=random(red,red);
+            fill(red,green,blue);
+            stroke(red,green,blue);
+            }
+        }
+      }
+      else{
+        if (flipNext){
+          beginShape();
+          vertex((i*triW),triH+(j*triH));
+          vertex(i*triW,j*triH);
+          vertex(triW+(i*triW),+triH+(j*triH));
+          vertex((i*triW),triH+(j*triH));
+          endShape();
+          flipNext=false;
+          fliped=false;
+          if (!spaces)i--;
+          if (!square){
+          var red=random(0,255);
+          var green=random(0,0);
+          var blue=random(red,red);
+          fill(red,green,blue);
+          stroke(red,green,blue);
+          }
+        }
+        else{
+          beginShape();
+          vertex(triW+(i*triW),triH+(j*triH));
+          vertex(triW+(i*triW),(j*triH));
+          vertex((i*triW),triH+(j*triH));
+          vertex(triW+(i*triW),triH+(j*triH));
+          endShape();
+          flipNext=true;
+          if (square){
+            var red=random(0,255);
+            var green=random(0,0);
+            var blue=random(red,red);
+            fill(red,green,blue);
+            stroke(red,green,blue);
+            }
+        }
+      }
+   }
+   if (allign){
+   if (fliped){
+    fliped=false;
+    var red=random(0,255);
+  var green=random(0,0);
+  var blue=random(red,red);
+  fill(red,green,blue);
+  stroke(red,green,blue);
+
+   }
+   else {
+    fliped=true;
+    var red=random(0,255);
+          var green=random(0,0);
+          var blue=random(red,red);
+          fill(red,green,blue);
+          stroke(red,green,blue);
+   } 
+  }
 }
-
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+  sleep(9999999999999999999999);
 }
